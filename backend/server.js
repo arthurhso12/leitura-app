d0af9456705df4aa0a32c350cf0ceb6a0289bc60
arthurhso14-web <arthurhso14@gmail.com>
2026-03-23 16:59:22 -0300

    corrigindo server.js

--- backend/server.js
+++ backend/server.js
@@ -3,31 +3,30 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+const app = express(); // ✅ PRIMEIRO
+
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log("Servidor rodando");
-});
-
-const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://admin:pomadinha@ac-y9c7cvt-shard-00-00.sqf65s6.mongodb.net:27017,ac-y9c7cvt-shard-00-01.sqf65s6.mongodb.net:27017,ac-y9c7cvt-shard-00-02.sqf65s6.mongodb.net:27017/?ssl=true&replicaSet=atlas-2u9ymb-shard-0&authSource=admin&appName=Cluster0")
+// CONEXÃO COM MONGO
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB conectado"))
   .catch(err => console.log(err));
 
+// ROTAS
 app.use("/auth", require("./routes/auth.routes"));
 app.use("/user", require("./routes/user.routes"));
 app.use("/books", require("./routes/book.routes"));
 app.use("/reading", require("./routes/reading.routes"));
 
+// ROTA TESTE
 app.get("/", (req, res) => {
   res.send("Backend funcionando 🚀");
 });
 
-app.listen(process.env.PORT || 5000, () =>
-  console.log("URI:", process.env.MONGO_URI)
-
-);
+// SERVIDOR
+app.listen(PORT, () => {
+  console.log("Servidor rodando 🚀");
+});
